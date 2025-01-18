a807d960e7bf53e0da480bdd8bdfd5b9749f6dd2
Khaled M. Abdelkader <m.abdelkader.khaled@gmail.com>
2025-01-18 15:33:56 +0200

    Create UI For server

---
+++ src/view/homeScreen/HomeScreenController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.homeScreen;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author yasse
+ */
+public class HomeScreenController implements Initializable {
+
+    @FXML
+    private Label homelbl;
+    @FXML
+    private Button startbtn;
+    @FXML
+    private Label serverlbl;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onStartClicked(ActionEvent event) {
+    }
+    
+}
