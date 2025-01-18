8b72c2ed8337aadc229f87ff29c7c10edaff975c
3wiida <mahmoudewida5@gmail.com>
2025-01-18 14:52:08 +0200

    init database connection

--- src/dao/DataAccessObject.java
+++ src/dao/DataAccessObject.java
@@ -5,10 +5,28 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.derby.jdbc.ClientDriver;
+
 /**
  *
  * @author 3wiida
  */
 public class DataAccessObject {
+    private static Connection connection;
+
     
+    static{
+        try {
+            DriverManager.registerDriver(new ClientDriver());
+            connection = DriverManager.getConnection("jdbc:derby://localhost:1527/TicTacToeDatabase", "root", "root");
+      
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
