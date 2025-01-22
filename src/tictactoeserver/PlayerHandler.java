a63f3b18aa00c6029bc2f871f6c3ca6702ec69f6
Mahmoud Ewida <mahmoudewida5@gmail.com>
2025-01-22 18:25:17 +0200

    Merge origin/development into development
    Conflicts:
    	src/tic_tac_toe/view/popups/serverConnectionpopup/ServerConnectionPopup.fxml
    	src/tic_tac_toe/view/popups/serverConnectionpopup/ServerConnectionPopupController.java

--- src/tictactoeserver/PlayerHandler.java
+++ src/tictactoeserver/PlayerHandler.java
@@ -73,6 +73,7 @@ public class PlayerHandler extends Thread {
                             JSONArray onlineUsers = new JSONArray();
 
                             for (Player player : onlinePlayers) {
+                                if (id .equals(player.getId())) continue;
                                 JSONObject playerJson = new JSONObject();
                                 playerJson.put("username", player.getUsername());
                                 playerJson.put("password", player.getPassword());
@@ -90,27 +91,27 @@ public class PlayerHandler extends Thread {
 
                         break;
                     }
-                    
+
                     case "invite": {
                         handleInvitationRequest(requestJson);
                         break;
                     }
-                    
-                    case "invitationRejected":{
+
+                    case "invitationRejected": {
                         handleInvitationRejectedRequest(requestJson);
                         break;
                     }
-                    
-                    case "invitationAccecpted":{
+
+                    case "invitationAccecpted": {
                         handleInvitationAccecptedRequest(requestJson);
                         break;
                     }
-                    
+
                     case "invitationCanceled": {
                         handleInvitationCanceled(requestJson);
                         break;
                     }
-                    
+
                     case "logout": {
                         handleLogoutRequest();
                         break;
@@ -126,17 +127,16 @@ public class PlayerHandler extends Thread {
             }
         }
     }
-    
-    
+
     /* Register Operations */
-    private void handleRegisterRequest(JSONObject request){
+    private void handleRegisterRequest(JSONObject request) {
         try {
             String id = UUID.randomUUID().toString();
             String username = request.getString("username");
             String password = request.getString("password");
             Player player = new Player(id, username, password, 0, 1);
             boolean isInsertionSuccess = DataAccessObject.insertPlayer(player);
-            if(isInsertionSuccess) {
+            if (isInsertionSuccess) {
                 this.id = id;
                 this.username = username;
             }
@@ -146,21 +146,20 @@ public class PlayerHandler extends Thread {
             sendRegisterResponse(false, null);
         }
     }
-    
-    private void sendRegisterResponse(boolean isSuccess, String username){
+
+    private void sendRegisterResponse(boolean isSuccess, String username) {
         JSONObject registerResonse = new JSONObject();
         registerResonse.put("isOk", isSuccess);
         registerResonse.put("username", username);
         registerResonse.put("id", id);
-        if(!isSuccess){
+        if (!isSuccess) {
             registerResonse.put("error", "User is already exists");
         }
         ps.println(registerResonse.toString());
     }
-    
-    
+
     /* Logout Operations */
-    private void handleLogoutRequest(){
+    private void handleLogoutRequest() {
         try {
             boolean isLogoutSuccess = DataAccessObject.updateUserStatus(id, 0);
             sendLogoutResponse(isLogoutSuccess);
@@ -169,28 +168,26 @@ public class PlayerHandler extends Thread {
             sendLogoutResponse(false);
         }
     }
-    
-    private void sendLogoutResponse(boolean isLogoutSuccess){
+
+    private void sendLogoutResponse(boolean isLogoutSuccess) {
         JSONObject logoutResponse = new JSONObject();
         logoutResponse.put("isOk", isLogoutSuccess);
         ps.println(logoutResponse.toString());
     }
-    
-    
-    
+
     /* Invitation System */
-    private void handleInvitationRequest(JSONObject invitation){
+    private void handleInvitationRequest(JSONObject invitation) {
         String opponentUsername = invitation.getString("opponentUsername");
-        for(PlayerHandler player : players){
+        for (PlayerHandler player : players) {
             System.out.println(player.username);
-            if(player.username.equals(opponentUsername)){
+            if (player.username.equals(opponentUsername)) {
                 sendInvitationToOpponent(this, player);
                 break;
             }
-        }   
+        }
     }
-    
-    private void sendInvitationToOpponent(PlayerHandler host, PlayerHandler opponent){
+
+    private void sendInvitationToOpponent(PlayerHandler host, PlayerHandler opponent) {
         JSONObject invitation = new JSONObject();
         invitation.put("type", "invitationRecieved");
         invitation.put("hostUsername", host.username);
@@ -199,56 +196,86 @@ public class PlayerHandler extends Thread {
         System.out.println("invitation is => " + invitation.toString());
         opponent.ps.println(invitation.toString());
     }
-    
-    private void handleInvitationRejectedRequest(JSONObject rejection){
+
+    private void handleInvitationRejectedRequest(JSONObject rejection) {
         String opponent = rejection.getString("from");
         String host = rejection.getString("to");
-        for(PlayerHandler player : players){
-            if(player.username.equals(host)){
+        for (PlayerHandler player : players) {
+            if (player.username.equals(host)) {
                 sendRejectionToHost(opponent, player);
                 break;
             }
-        }   
+        }
     }
-    
-    private void sendRejectionToHost(String opponent, PlayerHandler host){
+
+    private void sendRejectionToHost(String opponent, PlayerHandler host) {
         JSONObject rejection = new JSONObject();
         rejection.put("type", "invitationRejected");
         rejection.put("from", opponent);
         host.ps.println(rejection.toString());
     }
-    
-    private void handleInvitationAccecptedRequest(JSONObject confirmation){
+
+    private void handleInvitationAccecptedRequest(JSONObject confirmation) {
         String host = confirmation.getString("to");
         JSONObject opponentData = confirmation.getJSONObject("opponent");
-        for(PlayerHandler player : players){
-            if(player.username.equals(host)){
+        for (PlayerHandler player : players) {
+            if (player.username.equals(host)) {
                 sendAcceptanceToHost(opponentData, player);
                 break;
             }
         }
     }
-    
-    private void sendAcceptanceToHost(JSONObject opponentData, PlayerHandler host){
+
+    private void sendAcceptanceToHost(JSONObject opponentData, PlayerHandler host) {
         JSONObject accecptance = new JSONObject();
         accecptance.put("type", "invitationAccecpted");
         accecptance.put("opponent", opponentData);
         host.ps.println(accecptance.toString());
     }
-    
-    private void handleInvitationCanceled(JSONObject cancelation){
+
+    private void handleInvitationCanceled(JSONObject cancelation) {
         String opponent = cancelation.getString("to");
-        for(PlayerHandler player : players){
-            if(player.username.equals(opponent)){
+        for (PlayerHandler player : players) {
+            if (player.username.equals(opponent)) {
                 sendCancelToOpponent(player);
                 break;
             }
         }
     }
-    
-    private void sendCancelToOpponent(PlayerHandler player){
+
+    private void sendCancelToOpponent(PlayerHandler player) {
         JSONObject cancelation = new JSONObject();
         cancelation.put("type", "invitationCanceled");
         player.ps.println(cancelation.toString());
+    }
+
+    public static void broadcastMessage(String message) {
+        for (PlayerHandler player : players) {
+            player.ps.println(message);
+        }
+    }
+
+    public static void closeAllConnections() {
+        for (PlayerHandler player : players) {
+            player.closeConnection();
+            System.err.println("u are down");
+        }
+        players.clear();
+    }
+
+    public void closeConnection() {
+        try {
+            if (ps != null) {
+                ps.close();
+            }
+            if (dis != null) {
+                dis.close();
+            }
+            if (clientSocket != null) {
+                clientSocket.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(PlayerHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
